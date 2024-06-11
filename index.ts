8eb6036053ba0045c2eda7e28cbb84d9514b114c
szfa95 <szfa95@example.com>
2024-06-11 18:38:08 +0200

    festivalclass

--- index.ts
+++ index.ts
@@ -1,2 +1,44 @@
+import { Event } from "./src/classes/event";
+import { People } from "./src/classes/people";
 import { EventService } from "./src/interfaces/eventservice";
 import { UserService } from "./src/interfaces/userservice";
+
+export class Festival implements EventService, UserService{
+
+
+    addEvent(event: Event): void {
+        
+    }
+
+    removeEvent(event: Event): void {
+        
+    }
+
+    editEvent(event: Event): void {
+        
+    }
+
+    themeGrouping(event: Event): void {
+        
+    }
+
+    eventData(event: Event): void {
+        
+    }
+
+    addPeople(people: People): void {
+        
+    }
+
+    removePeople(people: People): void {
+        
+    }
+
+    editPeople(people: People): void {
+        
+    }
+
+    peopleData(people: People): void {
+        
+    }
+}
